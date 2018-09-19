7dfa0ab8c1c0c9bd273c8d6d3a67e90542c7e579
Lea Rosema <terabaud@gmail.com>
2018-09-19 23:05:34 +0200

    add identity object

--- server/src/middleware/authentication.php
+++ server/src/middleware/authentication.php
@@ -6,9 +6,11 @@ class Authentication {
 
   public function __construct($container) {
     $this->db = $container['db'];
+    $this->container = $container;
   }
 
   public function authenticate($id, $password) {
+    // TODO: Move authentication out into a user service class
     $query = $this->db->prepare('SELECT COUNT(*) AS count FROM `users` WHERE id = :id AND password = :password');
     $query->execute([':id' => $id, ':password' => $password]);
     $row = $query->fetch();
@@ -22,13 +24,17 @@ class Authentication {
     if ($authorization) {
       list ($authType, $code) = explode(' ', $authorization);
       list ($user, $passwd) = explode(':', base64_decode($code));
-      $authenticated = $this->authenticate($user, $passwd);      
+      $authenticated = $this->authenticate($user, $passwd);
+      if ($authenticated) {
+        $this->container['identity'] = [ 'id' => $user, 'name' => $user ];
+      }
     }
     if (! $authenticated) {
       $response = $response->withStatus(401);
       $response->getBody()->write('Access denied.');
       return $response;
     }
+    
     $response = $next($request, $response);
     return $response;
   }
