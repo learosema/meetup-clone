68e3dbce3acae339fda1e2d46ae4bdfd3a6697b2
Lea Rosema <terabaud@gmail.com>
2018-09-29 01:51:40 +0200

    test post user

--- server/tests/Functional/HomepageTest.php
+++ server/tests/Functional/HomepageTest.php
@@ -4,23 +4,40 @@ namespace Tests\Functional;
 
 class HomepageTest extends BaseTestCase
 {
-    /**
-     * Test that the index route redirects to the Swagger OpenAPI documentation
-     */
-    public function testGetHomepageWithoutName()
-    {
-        $response = $this->runApp('GET', '/');
-        $this->assertEquals(301, $response->getStatusCode());
-    }
+  
+  // The index route should redirect to the Swagger OpenAPI documentation
+  public function testGetIndex()
+  {
+    $response = $this->runApp('GET', '/');
+    $this->assertEquals(301, $response->getStatusCode());
+  }
 
-    /**
-     * Test that the index route won't accept a post request
-     */
-    public function testPostHomepageNotAllowed()
-    {
-        $response = $this->runApp('POST', '/', ['test']);
+  // POST / should return Method not allowed
+  public function testPostHomepageNotAllowed()
+  {
+    $response = $this->runApp('POST', '/', ['test']);
+    $this->assertEquals(405, $response->getStatusCode());
+    $this->assertContains('Method not allowed', (string)$response->getBody());
+  }
 
-        $this->assertEquals(405, $response->getStatusCode());
-        $this->assertContains('Method not allowed', (string)$response->getBody());
-    }
+  // GET /users should return an array of users
+  public function testGetUsers() {
+    $response = $this->runApp('GET', '/users');
+    $this->assertEquals(200, $response->getStatusCode());
+    $data = json_decode($response->getBody());
+    $this->assertEquals(1, count($data));
+    $this->assertEquals('Administrator', $data[0]->name);
+  }
+
+  // POST /user/{id} should create a new user
+  public function testPostUser() {
+    $response = $this->runApp('POST', '/user/lea', [
+      'password' => 'lea',
+      'name' => 'Lea Rosema',
+      'email' => 'lea@lea.de',
+    ]);
+    $this->assertEquals(200, $response->getStatusCode());
+    $data = json_decode($response->getBody());
+  }
+
 }
