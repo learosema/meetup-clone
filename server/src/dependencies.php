46a2d1e53a17fdb78beeeaa9c4f8713139cbab74
Lea Rosema <terabaud@gmail.com>
2018-09-29 01:31:29 +0200

    add test data for testing when testing

--- server/src/dependencies.php
+++ server/src/dependencies.php
@@ -14,6 +14,9 @@ $container['db'] = function ($c) {
   } catch (PDOException $ex) {
     $db->exec(file_get_contents(__DIR__ . '/../db/schema.sql'));
     $db->isFreshDB = true;
+    if ($db_config['connection'] === 'sqlite::memory:') {
+      $db->exec(file_get_contents(__DIR__ . '/../db/testdata.sql'));
+    }
   }
   return $db;
 };
@@ -42,4 +45,3 @@ $container['env'] = function ($c) {
   }
   return (object)$data;
 };
-
