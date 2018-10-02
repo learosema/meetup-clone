97fe0af67ddf732c5b17e69637bb21e60e3dfd2c
Lea Rosema <terabaud@gmail.com>
2018-10-02 17:49:51 +0200

    test update

--- server/tests/Functional/ApiTest.php
+++ server/tests/Functional/ApiTest.php
@@ -178,5 +178,21 @@ class ApiTest extends BaseTestCase
     $this->assertEquals(200, $response->getStatusCode());
   }
 
+// PUT /group/{id}/event/{id} should create an event
+public function testUpdateGroupEvent() {
+  $response = $this->runApp('PUT', '/group/test/event/test-1', [
+    'id' => 'test-1',
+    'group_id' => 'test lalala',
+    'name' => 'Test Hello World',
+    'description' => 'Lorem ipsum dolor sit amet consectetur, adipisicing elit.',
+    'location' => 'Dummy Place',
+    'address' => 'South Pole',
+    'lat' => '0',
+    'lon' => '90',
+    'date' => '2018-10-31'
+  ], ['user' => 'lea', 'password' => 'lea123']);
+  $this->assertEquals(200, $response->getStatusCode());
+}
+
 
 }
